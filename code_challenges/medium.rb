8966e79a44f27c055d95d4171f49cdc7e86f7503
Kailey Kaes <56466345+kaileykaes@users.noreply.github.com>
2022-12-07 13:49:03 -0700

    Add conditional statement

---
+++ code_challenges/medium.rb
@@ -0,0 +1,24 @@
+#Write a program that evaluates the status of the variables above, 
+#and uses logic to print one of three possible messages:
+
+#If the driving record is good and user is over 25 years old, 
+#they should get a discount on the car rental
+
+#If the user either has a good record or is over 25 years old, 
+#they should pay full price
+
+#If the user is not over 25 and has a bad driving record, 
+#they need to have someone else sign for the rental
+
+good_driving_record = true
+age = 24
+
+
+
+if good_driving_record == true && age > 25
+    puts 'You qualify for a discount!'
+elsif good_driving_record == true || age > 25
+    puts 'Your rental will cost 1 bajillion.'
+else 
+    puts 'Warm up your chevrolegs, friend!'
+end 
