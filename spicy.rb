2a9391d83efa083d515d8367f7832ebb2ef5a1e0
Kailey Kaes <56466345+kaileykaes@users.noreply.github.com>
2022-12-07 13:07:43 -0700

    Retest for shiggles

--- spicy.rb
+++ spicy.rb
@@ -6,7 +6,7 @@
 #If the Integer is a multiple of both 3 and 5, print "FizzBuzz". 
 #If the Integer is not a multiple of either, print the Integer itself.
 
-mystery = 31
+mystery = 92
 
 if mystery % 3 == 0 && mystery % 5 == 0 
     puts 'fizzbuzz'
