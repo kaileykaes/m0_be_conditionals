88c9423e112812c49e77a6299f93437ca1c9daf6
Kailey Kaes <56466345+kaileykaes@users.noreply.github.com>
2022-12-07 13:30:24 -0700

    Write conditional

---
+++ code_challenges/mild.rb
@@ -0,0 +1,13 @@
+#Write a Ruby program that defines a variable that stores an Integer.
+# The program should print out the String "even" if the Integer is even, 
+#and the String "odd" if the Integer is odd.
+
+var = 5 
+
+if var.even? == true
+    puts 'even'
+else
+    puts 'odd'    
+end
+
+
