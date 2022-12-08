3a7a770da30276db7d9f0590d3761c4610c0016a
Kailey Kaes <56466345+kaileykaes@users.noreply.github.com>
2022-12-07 17:59:02 -0700

    Write conditional

---
+++ code_challenges/spicy_share.rb
@@ -0,0 +1,11 @@
+mystery = 67
+
+if mystery % 3 == 0 && mystery % 5 !=0
+    puts "Fizz"
+elsif mystery % 5 == 0 && mystery % 3 != 0 
+    puts "Buzz"
+elsif mystery % 5 == 0 && mystery % 3 == 0
+    puts "Fizzbuzz"
+else 
+    puts #{mystery}
+end 
